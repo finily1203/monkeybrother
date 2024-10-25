a43812ed7904289a457cba7244852ffad911d090
Ian Loi <ianloi382@gmail.com>
2024-10-26 00:00:54 +0800

    Fix an error when merging previously
    Remove unnecessary codes that causes errors

--- Sandbox/ECS/ECSCoordinator.cpp
+++ Sandbox/ECS/ECSCoordinator.cpp
@@ -32,50 +32,6 @@ All content @ 2024 DigiPen Institute of Technology Singapore, all rights reserve
 #include <Windows.h>
 
 #include "GlobalCoordinator.h"
-
-
-// this function retrieves the executable path based on your desktop
-// dynamic path retrieval
-std::string ECSCoordinator::GetExecutablePath()
-{
-	char buffer[MAX_PATH];
-
-	// this is a windows API function that will retrieve the fully
-	// qualified path of the executable file 
-	GetModuleFileNameA(nullptr, buffer, MAX_PATH);
-
-	// initialize the full executable path to fullPath
-	std::string fullPath(buffer);
-
-	// return the directory path by extracting a substring from the
-	// fullPath
-	return fullPath.substr(0, fullPath.find_last_of("\\/"));
-}
-
-// this function retrieves the windowsConfig JSON file
-std::string ECSCoordinator::GetWindowConfigJSONPath()
-{
-	// retrieves the executable path
-	std::string execPath = GetExecutablePath();
-	
-	// retrieves the windowsConfig JSON file path
-	std::string jsonPath = execPath.substr(0, execPath.find_last_of("\\/")) + "\\..\\..\\Sandbox\\Serialization\\windowConfig.json";
-
-	return jsonPath;
-}
-
-// this function retrieves the entites JSON file
-std::string ECSCoordinator::GetEntitiesJSONPath()
-{
-	// retrieves the executable path
-	std::string execPath = GetExecutablePath();
-
-	// retrieves the entities JSON file path
-	std::string jsonPath = execPath.substr(0, execPath.find_last_of("\\/")) + "\\..\\..\\Sandbox\\Serialization\\entities.json";
-
-	return jsonPath;
-}
-
 #include <random>
 #include <glm/glm.hpp>
 
