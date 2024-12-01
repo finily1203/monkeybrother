0db55eeda671b6bc631f5b457df5f40d6f5fe019
Ian Loi <ianloi382@gmail.com>
2024-12-01 21:03:57 +0800

    Pump behavior update

--- Sandbox/SystemECS/EffectPumpBehaviour.cpp
+++ Sandbox/SystemECS/EffectPumpBehaviour.cpp
@@ -32,28 +32,33 @@ void EffectPumpBehaviour::update(Entity entity) {
 	}
 
 	if (GLFWFunctions::isPumpOn) {
-		auto playerEntity = ecsCoordinator.getEntityFromID("player");;
+		auto playerEntity = ecsCoordinator.getEntityFromID("player");
+		auto bubblesEntity = ecsCoordinator.getEntityFromID("bubbles 3.png");
 		auto PhysicsSystemRef = ecsCoordinator.getSpecificSystem<PhysicsSystemECS>();
 		auto collisionSystem = PhysicsSystemRef->getCollisionSystem();
 
 		auto& playerTransform = ecsCoordinator.getComponent<TransformComponent>(playerEntity);
+		auto& bubblesTransform = ecsCoordinator.getComponent<TransformComponent>(bubblesEntity);
 		myMath::Vector2D& playerPos = playerTransform.position;
 		float radius = playerTransform.scale.GetX() * 0.5f;
 
 		CollisionSystemECS::OBB playerOBB = collisionSystem.createOBBFromEntity(playerEntity);
-		CollisionSystemECS::OBB pumpOBB = collisionSystem.createOBBFromEntity(entity);
+		//CollisionSystemECS::OBB pumpOBB = collisionSystem.createOBBFromEntity(entity);
+		CollisionSystemECS::OBB bubblesOBB = collisionSystem.createOBBFromEntity(bubblesEntity);
 
 		myMath::Vector2D normal{};
 		float penetration{};
-		bool isColliding = collisionSystem.checkCircleOBBCollision(playerPos, radius, pumpOBB, normal, penetration);
+		bool isColliding = collisionSystem.checkCircleOBBCollision(playerPos, radius, bubblesOBB, normal, penetration);
 
 		if (isColliding) {
+			myMath::Vector2D bubblesDirectionalVec = PhysicsSystemRef->directionalVector(bubblesTransform.orientation.GetX());
 			auto& playerPhysics = ecsCoordinator.getComponent<PhysicsComponent>(playerEntity);
 			float pumpForce = ecsCoordinator.getComponent<PumpComponent>(entity).pumpForce;
 			float mag = playerPhysics.force.GetMagnitude() * pumpForce;
 			ForceManager forceManager = ecsCoordinator.getComponent<PhysicsComponent>(playerEntity).forceManager;
 
-			forceManager.AddForce(playerEntity, myMath::Vector2D(-mag, -mag));
+			//forceManager.AddForce(playerEntity, myMath::Vector2D(-mag, -mag));
+			forceManager.AddForce(playerEntity, bubblesDirectionalVec);
 
 			std::cout << "Collision" << std::endl;
 		}
