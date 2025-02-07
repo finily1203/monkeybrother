93f89eddc6b3e8631f4d7ced06a5f170907ff246
LZHOwen <owenlew00@gmail.com>
2025-02-07 08:34:45 +0800

    disable imgui

--- Sandbox/GLFWFunctions/GlfwFunctions.cpp
+++ Sandbox/GLFWFunctions/GlfwFunctions.cpp
@@ -240,10 +240,10 @@ void GLFWFunctions::keyboardEvent(GLFWwindow* window, int key, int scancode, int
 
     if (!GameViewWindow::getPaused()) {
 
-        if ((*keyState)[Key::NUM_1]) {
+        /*if ((*keyState)[Key::NUM_1]) {
             debug_flag = !debug_flag;
             isGuiOpen = ~isGuiOpen;
-        }
+        }*/
     }
 
     if ((*keyState)[Key::A] || (*keyState)[Key::D]) {
