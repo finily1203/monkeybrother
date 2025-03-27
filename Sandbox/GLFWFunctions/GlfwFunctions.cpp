ef78add8b43170088d6d5ef8b384ab2172db45b0
Joel-cwy <149317102+Joelcwy-Digipen@users.noreply.github.com>
2025-03-27 23:33:15 +0800

    removed shift
    oops

--- Sandbox/GLFWFunctions/GlfwFunctions.cpp
+++ Sandbox/GLFWFunctions/GlfwFunctions.cpp
@@ -257,12 +257,6 @@ void GLFWFunctions::keyboardEvent(GLFWwindow* window, int key, int scancode, int
 
 #endif
 
-    if ((*keyState)[Key::LSHIFT])
-    {
-        GameViewWindow::setSceneNum(-4);
-        ecsCoordinator.LoadEndCutsceneFromJSON(ecsCoordinator, FilePathManager::GetEndCutsceneJSONPath());
-    }
-
     if ((*keyState)[Key::A] || (*keyState)[Key::D]) {
         isRotating = true;
     }
